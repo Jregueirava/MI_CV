fd7588831bf7651b0f9d39f23b3c4e4586eb3fbe
jesus <jesusregueiravazquez@gmail.com>
2026-02-23 20:53:47 +0100

    .

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1092698309"
+    serviceWorkerVersion: "3904030640"
   }
 });
